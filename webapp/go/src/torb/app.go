d5a0082e9ee4f492381a531335ee569a20358b60
takoshi <t.pro.6q@gmail.com>
2020-08-15 22:08:42 +0900

    move roolback

--- webapp/go/src/torb/app.go
+++ webapp/go/src/torb/app.go
@@ -720,14 +720,15 @@ func main() {
 		var sheet Sheet
 		var reservationID int64
 		for {
+			tx, err := db.Begin()
 			if err := db.QueryRow("SELECT * FROM sheets WHERE id NOT IN (SELECT sheet_id FROM reservations WHERE event_id = ? AND canceled_at IS NULL FOR UPDATE) AND `rank` = ? ORDER BY RAND() LIMIT 1", event.ID, params.Rank).Scan(&sheet.ID, &sheet.Rank, &sheet.Num, &sheet.Price); err != nil {
+				tx.Rollback()
 				if err == sql.ErrNoRows {
 					return resError(c, "sold_out", 409)
 				}
 				return err
 			}
 
-			tx, err := db.Begin()
 			if err != nil {
 				return err
 			}
